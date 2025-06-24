8a81dbde5fb3bd59c7f8a4ece435435f80b9b621
haRu-pp929 <hrk.mt0929@gmail.com>
2025-06-24 15:15:10 +0900

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,7 +5,7 @@
       const password = document.getElementById('password').value;
 
       // デモ用：ユーザー名「admin」、パスワード「1234」で成功
-      if (username === 'admin' && password === '1234') {
+      if (username === 'laetitia' && password === 'aroma') {
         window.location.href = 'main.html'; // メインページへ遷移
       } else {
         document.getElementById('error-msg').style.display = 'block';
